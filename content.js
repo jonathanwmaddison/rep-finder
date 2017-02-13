2701b952b28b9fb2809e9f73e2544acf185b4f21
Isabel Kloumann <imk36@cornell.edu>
2017-02-12 23:50:06 -0800

    added news feature

--- content.js
+++ content.js
@@ -8,15 +8,11 @@ function getData (zip) {
   var count=0
   var data;
   var appendData = "";
+  var collapseId=0;
   $.getJSON(url, function(data){
     console.log(data);
     appendData+="<div class=\"repDisplay\">"  ;
-    
     for (division in data.divisions) {
-      if (count>0) {
-        appendData+="</div>";
-      }
-      count++;
       appendData +="<div class=division><h1>"+data.divisions[division].name+"</h1>";
       data.divisions[division].officeIndices.forEach(function(office){
         data.offices[office].officialIndices.forEach(function(official){
@@ -26,7 +22,8 @@ function getData (zip) {
           var website = "";
           var photo = "";
           var socialMedia="<div class=\"col-sm-3\"><h4> Social Media</h4><ul>";
-          var newsButton =  "<br><button id=\"" + encodeURIComponent(data.officials[official].name) + "\" type=\"submit\" class=\"btn btn-lg btn-danger get-news\" data-toggle=\"popover\" title=\"Recent News\" data-content=\"\">Get News</button>";
+          var newsButton =  "<div class=\"row\"><div class=\"panel-group\" id=\""+ data.officials[official].name.replace(/ /g,"-").replace(/\./g,"_")+"\" role=\"tablist\" aria-multiselectable=\"true\"><div class=\"panel panel-default\"><div class=\"panel-heading\" role=\"tab\" id=\"heading"+collapseId+"\"><h4 class=\"panel-title\"><a role=\"button\" data-toggle=\"collapse\" data-parent=\"#"+ data.officials[official].name.replace(/ /g,"-").replace(/\./g,"_")+"\" href=\"#collapse"+collapseId+"\" aria-expanded=\"true\" aria-controls=\"collapse"+collapseId+"\">News</a></h4></div><div id=\"collapse"+collapseId+"\" class=\"panel-collapse collapse\" role=\"tabpanel\" aria-labelledby=\"headingOn\"><div class= \"panel-body\"id=\"text"+ data.officials[official].name.replace(/ /g,"-").replace(/\./g,"_")+"\"></div></div></div></div></div>"
+          collapseId++;
           if (data.officials[official].phones) {
             officialPhone = "<div class=\"phone\"> Phone: " + data.officials[official].phones[0] + "</div>"
           }
@@ -35,6 +32,9 @@ function getData (zip) {
             socialMedia += data.officials[official].channels[1] ? "<li>"+ data.officials[official].channels[1].type +": "+ data.officials[official].channels[1].id + "</li>" : "";
             socialMedia += data.officials[official].channels[2] ? "<li>"+ data.officials[official].channels[2].type +": "+ data.officials[official].channels[2].id + "</li>" : "";
             socialMedia+="</ul></div>";
+          }
+          else {
+            socialMedia+="</div>"
           }
           if(data.officials[official].address) {
             officialContact += data.officials[official].address[0].line1 ? data.officials[official].address[0].line1 + ", " : "";
@@ -52,19 +52,16 @@ function getData (zip) {
             appendData += "<div class=\"row\"><div class=\" col-sm-3 photo\"><img id = \"repPhoto\" src=\"" + photo + "\" alt=\" Photo of Rep\" class=\"img-thumbnail\"></div>"
           } else {
             photo = "http://i.imgur.com/iMTIAcQ.jpg"
-            appendData += "<div class=\"row\"><div class=\" col-sm-3 photo\"><img id = \"repPhoto\" src=\"" + photo + "\" alt=\" Photo of Rep\" class=\" img-thumbnail\">"+newsButton+"</div>"
+            appendData += "<div class=\"row\"><div class=\" col-sm-3 photo\"><img id = \"repPhoto\" src=\"" + photo + "\" alt=\" Photo of Rep\" class=\" img-thumbnail\"></div>"
           }
-          appendData+="<div class=\" col-sm-3 address\"> <h4> Mailing Address </h4><p>" + officialContact + "</p></div><div class=\" contact col-sm-3\"><h4> Contact </h4> <p>" +officialPhone + website + " </p></div>"+socialMedia+"</div></div>"
+          appendData+="<div class=\" col-sm-3 address\"> <h4> Mailing Address </h4><p>" + officialContact + "</p></div><div class=\" contact col-sm-3\"><h4> Contact </h4> <p>" +officialPhone + website + " </p></div>"+socialMedia+"</div>"+newsButton+"</div>"
         })
-      })      
+      })
+    appendData+="</div>" 
     }
     appendData+="</div>"
     $(".container").append(appendData);
-    
-    $(function () {
-      $('[data-toggle="popover"]').popover()
-    })
-    $( ".get-news" ).on( "click", function() {
+    $( ".panel-group" ).on( "click", function() {
       getNews( $( this ).attr("id") );
     });
     localStorage['append'] = appendData;
@@ -72,33 +69,38 @@ function getData (zip) {
 }
 
 function getNews(id) {
-  
-  var googleNewsRss= "https://news.google.com/news?search="+id+"&output=rss";
-  
+  if($("#content"+id).length>0) {
+     return
+  }
+  var contentForDisplay = "<span id=\"content"+ id +"\"><ol>";
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': "b3947cbe5bfb4ffeac69be6569aa335f",
+    'q': encodeURIComponent(id.replace(/-/g," ").replace(/_/g,".")),
+    'sort': "newest"
+  });
   $.ajax({
-    type: "GET",
-    headers: {
-      "Access-Control-Allow-Origin": 1,
-  },
-    url: googleNewsRss
-    }).done(function (data) {
-      console.log(data);
-    });
+    url: url,
+    method: 'GET',
+  }).done(function(result) {
+    result.response.docs.forEach(function (article) {
+      contentForDisplay += "<li><a href="+ article.web_url+">"+ article.headline.main+"</a></li>"
+    })
+    $("#text"+id).append(contentForDisplay+"</ol></span>");
+  }).fail(function(err) {
+    throw err;
+  });   
 }
 
 $(document).ready(function() {
   if(localStorage["append"]) {
     $(".container").append(localStorage["append"]);
-    $(function () {
-      $('[data-toggle="popover"]').popover()
-    })
   }
-  $( ".get-news" ).on( "click", function() {
+  $( ".panel-group" ).on( "click", function() {
     getNews($( this ).attr("id") );
   });
   $( "#zipForm" ).submit(function( event ) {
       $(".repDisplay").remove()
-    console.log("test")
     var zip = $("#zip" ).val()
     $("#zip" ).val("")
     getData(zip);
