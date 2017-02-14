514d5863188e712acb4faf9674e3621c792f3add
Isabel Kloumann <imk36@cornell.edu>
2017-02-14 15:05:59 -0800

    added twitter feed

--- content.js
+++ content.js
@@ -1,5 +1,25 @@
 //Bootstrap col size for address,photo, etc. In case it needs to be adjusted.
 var columnSize = "col-sm-3";
+var panelSize  = "col-sm-6";
+var twitterHandle = "";
+var twitterTracker = {};
+//include twitter embed JS -> https://dev.twitter.com/web/javascript/loading
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
 
 //store data from representative search. Google civic info API provides data!
 
@@ -12,7 +32,6 @@ function getData (address) {
   $.getJSON(url, function(data){
     localStorage["data"] = JSON.stringify(data);
     formatAndAppend(data)
-
   })
 }
 ///////// Helper functions to format data ////////
@@ -22,6 +41,7 @@ function formatSocialButton (channel){
     return  "<a target=\"_blank\" href =\"https://www.facebook.com/"+ channel.id +"/\"><img  class=\"socialIcon\" src=\"images/"+ channel.type.toLowerCase()+".svg\"></a></li>"
   }
   else if (channel.type === "Twitter"){
+    twitterHandle = channel.id;
     return  "<a target=\"_blank\" href =\"https://www.twitter.com/"+ channel.id +"/\"><img  class=\"socialIcon\" src=\"images/"+ channel.type.toLowerCase()+".svg\"></a></li>"
 
   }
@@ -87,31 +107,42 @@ function formatSocial(official) {
   }
   return socialMedia;
 }
-function formatNews(official, id){
-  return "<div class=\"row\"><div class=\"panel-group\" id=\""+ official.name.replace(/ /g,"-").replace(/\./g,"_")+"\" role=\"tablist\" aria-multiselectable=\"true\"><div class=\"panel panel-default\"><div class=\"panel-heading\" role=\"tab\" id=\"heading"+id+"\"><h4 class=\"panel-title\"><a class=\"panel-link\" role=\"button\" data-toggle=\"collapse\" data-parent=\"#"+ official.name.replace(/ /g,"-").replace(/\./g,"_")+"\" href=\"#collapse"+id+"\" aria-expanded=\"true\" aria-controls=\"collapse"+id+"\">News <span class=\" glyphicon glyphicon-menu-hamburger\"></span></a></h4></div><div id=\"collapse"+id+"\" class=\"panel-collapse collapse\" role=\"tabpanel\" aria-labelledby=\"headingOn\"><div class= \"panel-body\"id=\"text"+ official.name.replace(/ /g,"-").replace(/\./g,"_")+"\"></div></div></div></div></div>"
+function formatNews(official, id, twitterHandle){
+  if(twitterHandle === "") {
+    var size = "col-sm-12";
+  } else {
+    size = panelSize
+  }
+  return "<div class=\""+size+" expand-row\"><div class=\"panel-group news-search\" id=\""+ official.name.replace(/ /g,"-").replace(/\./g,"_")+"\" role=\"tablist\" aria-multiselectable=\"true\"><div class=\"panel panel-default\"><div class=\"panel-heading\" role=\"tab\" id=\"heading"+id+"\"><h4 class=\"panel-title\"><a class=\"panel-link\" role=\"button\" data-toggle=\"collapse\" data-parent=\"#"+ official.name.replace(/ /g,"-").replace(/\./g,"_")+"\" href=\"#collapse"+id+"\" aria-expanded=\"true\" aria-controls=\"collapse"+id+"\"><span class=\"news-button-text\"> News <span class=\" glyphicon glyphicon-menu-hamburger\"></span></span></a></h4></div><div id=\"collapse"+id+"\" class=\"panel-collapse collapse\" role=\"tabpanel\" aria-labelledby=\"headingOn\"><div class= \"panel-body\"id=\"text"+ official.name.replace(/ /g,"-").replace(/\./g,"_")+"\"></div></div></div></div></div>"
+}
+function formatTwitter(official, id){
+  return "<div class=\""+panelSize+" expand-row\"><div class=\"panel-group twitter-feed\" id=\""+ twitterHandle+"\" role=\"tablist\" aria-multiselectable=\"true\"><div class=\"panel panel-default\"><div class=\"panel-heading\" role=\"tab\" id=\"twitterheading"+id+"\"><h4 class=\"panel-title\"><a class=\"panel-link\" role=\"button\" data-toggle=\"collapse\" data-parent=\"#"+ twitterHandle+"\" href=\"#collapse"+id+"\" aria-expanded=\"true\" aria-controls=\"collapse"+id+"\"><span class=\"news-button-text\"> Recent Tweets <span class=\" glyphicon glyphicon-menu-hamburger\"></span></span></a></h4></div><div id=\"collapse"+id+"\" class=\"panel-collapse collapse\" role=\"tabpanel\" aria-labelledby=\"headingOn\"><div class= \"panel-body\"id=\"embed"+ twitterHandle+"\"></div></div></div></div></div>"
 }
 /////////////////////End Helper functions
 
 //Utilizes data from google and helper functions to format data for display.
 function formatAndAppend (data) {
+
   var officeName= "";
   var appendData = "";
   collapseId=0;
   appendData+="<div class=\"repDisplay\">";
   for (division in data.divisions) {
     if(!data.divisions[division].officeIndices) {
-      console.log("empty division!")
     } else {
     appendData +="<div class=division><h1>"+data.divisions[division].name+"</h1>";
     data.divisions[division].officeIndices.forEach(function(office){
       officeName = data.offices[office].name;
       data.offices[office].officialIndices.forEach(function(official){
+        twitterHandle = "";
         var name = formatName(data.officials[official], officeName)
         var photo = formatPhoto(data.officials[official])
         var address = formatAddress(data.officials[official])
         var contact = formatContact(data.officials[official])
         var socialMedia = formatSocial(data.officials[official])
-        var newsButton = formatNews(data.officials[official], collapseId)
+        var newsButton = formatNews(data.officials[official], collapseId, twitterHandle)
+
+        console.log(twitterHandle)
         //id counter for newsButton that ensures each accordian panel is unique
         collapseId++;
         
@@ -119,9 +150,16 @@ function formatAndAppend (data) {
         appendData+= name + photo + address + contact + socialMedia + "</div>";
 
         //close out official with NewsButton
-        appendData+=newsButton
+        appendData+="<div class=\"row tweetandnewsrow\">"+newsButton 
+        if(twitterHandle != "") {
+          var twitterButton= formatTwitter(data.officials[official], collapseId)
+          appendData += twitterButton +"</div>";
+        } else {
+          appendData += "</div>";
+        }
       })
       //close out office
+      appendData+="</div>"
     })
   }
     //close out division
@@ -132,9 +170,16 @@ function formatAndAppend (data) {
   localStorage["append"] = appendData;
 
   $(".container").append(appendData);
-  $( ".panel-group" ).on( "click", function() {
+  $( ".news-search" ).on( "click", function() {
       getNews( $( this ).attr("id") );
     });
+  $( ".twitter-feed" ).on( "click", function() {
+    console.log($("embed"+$( this ).attr("id")).children(".twitter-timeline twitter-timeline-rendered"))
+      if (!twitterTracker[$( this ).attr("id")]) {
+      twitterEmbed( $( this ).attr("id") );
+      twitterTracker[$( this ).attr("id")] = true;
+     }
+  });
 }
 
 //grabs news from NY times API
@@ -143,7 +188,6 @@ function getNews(id) {
   if($("#content"+id).length>0) {
      return
   }
-  console.log(id.replace(/-/g,"+").replace(/_/g,""))
   var contentForDisplay = "<span id=\"content"+ id +"\"><ol>";
   var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 
@@ -165,7 +209,19 @@ function getNews(id) {
   });   
 }
 
-
+function twitterEmbed(handle) {
+  twttr.widgets.createTimeline(
+  {
+    sourceType: 'profile',
+    screenName: handle
+  },
+  document.getElementById("embed"+handle),
+  {
+    width: '450',
+    height: '30.5rem',
+  }).then(function (el) {
+  });
+}
 //Functions that run once the document is loaded
 $(document).ready(function() {
 
@@ -178,16 +234,6 @@ $(document).ready(function() {
     formatAndAppend(JSON.parse(localStorage["data"]))
   }
 
-  // event listener for panel groups
-  // $( ".panel-group" ).on( "click", function() {
-  //   console.log($( this ).attr("id"));
-  //   getNews($( this ).attr("id") );
-  // });
-
-  $(".myBox").click(function() {
-  window.location = $(this).find("a").attr("href"); 
-  return false;
-})
   // event listener for rep search form
   $( "#zipForm" ).submit(function( event ) {
       $(".repDisplay").remove()
