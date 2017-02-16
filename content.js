aa92ae3225fa8abac7b4441381e1cb307fc687b9
Isabel Kloumann <imk36@cornell.edu>
2017-02-15 16:51:25 -0800

    fix wiki api https issue

--- content.js
+++ content.js
@@ -268,7 +268,7 @@ function getWikiPageName (name) {
 function getWikiPageData (pageName, id){
   $.ajax({
     type: "GET",
-    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+pageName+"&callback=?",
+    url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+pageName+"&callback=?",
     contentType: "application/json; charset=utf-8",
     async: false,
     dataType: "json",
